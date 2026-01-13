13f17ad2e01400f7e57e4db45d39673f33cc1256
connection-establish <bn627862@gmail.com>
2026-01-13 22:56:39 +0530

    ✨starting to do a chalenge

---
+++ Chalenges/tic_tac_tor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int x,i;
+    x=1;
+    printf("%d",&x);
+    while (x==9){
+        i=1;
+        while(i==3){
+            printf("%d",&i);
+        }
+        
+    }
+    
+}
