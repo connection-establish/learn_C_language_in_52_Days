7a00386790529e84e31efac722ee8ec6537e38d4
connection-establish <bn627862@gmail.com>
2026-01-14 06:33:18 +0530

    ✨update

--- Chalenges/tic_tac_tor.c
+++ Chalenges/tic_tac_tor.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
 int main(){
-    int row , col;
-    row =1,col =1;
-    while (row<=4){
-        printf("-- "); 
-        row++;
-    }
+    int row , col, num;
+    row =1 ,num =1;
+        while (row<=3){
+            col = 1;              
+            while (col <= 3) {
+                printf(" _ ");
+                col++;
+            }
+            printf("  _ ");
+            printf("\n");
+            col=1;
+            while (col<=3)
+            {
+               printf("| %d ", num);
+               num++;
+               col++;
+            }
+            row++;
+            printf("|\n");
+        }
+        printf(" -  -   -   -  \n");
     return 0;
-    
 }
